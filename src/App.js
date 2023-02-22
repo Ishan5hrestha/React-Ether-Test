b7a30d5bc3e86cecefd2e5495259157b00aff071
ishan5hrestha <ishanshrestha@gmail.com>
2023-02-22 23:47:41 +0545

    automatic connection

--- src/App.js
+++ src/App.js
@@ -11,6 +11,49 @@ function App() {
   const [meroAddress, setMeroAddress] = useState("Biratnagar");
   const [address, setAddress] = useState("0x00000000");
 
+  const [contract, setContract] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [providerStats, setProviderStats] = useState("Loading");
+
+  const contractAddress = ''
+
+  useEffect(() =>{
+    if (provider && !contract) console.log('sabai aaisakyo');
+    else init();
+    console.log('init called');
+  },[provider])
+
+  const init = async () => {
+    // Return the provider or null if it isn't detected
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      setProviderStats("Ready");
+      provider.enable();
+      await provider.request({ method: 'eth_requestAccounts' });
+      const signer = new ethers.providers.Web3Provider(provider).getSigner();
+      const address = await signer.getAddress();
+      setAddress(address);
+
+      // Note that this event is emitted on page load.
+      // If the array of accounts is non-empty, you're already
+      // connected.
+      provider.on("accountsChanged", handleAccountsChanged);
+
+
+      // For now, 'eth_accounts' will continue to always return an array
+      function handleAccountsChanged(accounts) {
+        if (accounts.length === 0) {
+          // MetaMask is locked or the user has not connected any accounts
+          console.log("Please connect to MetaMask.");
+        } else if (accounts[0] !== address) {
+          setAddress(accounts[0]);
+        }
+      } 
+    } else {
+        setAddress('Please install MetaMask');
+    }
+  }
+
   const reactiveness = () => {
     setMeroAddress("New Address");
   };
@@ -18,47 +61,25 @@ function App() {
   async function connectWallet() {
     try {
       const provider = await detectEthereumProvider();
-      console.log(provider);
       if (provider) {
-      console.log(provider);
-
         await provider.request({ method: 'eth_requestAccounts' });
         const signer = new ethers.providers.Web3Provider(provider).getSigner();
         const address = await signer.getAddress();
         setAddress(address);
       } else {
-        console.log('Please install MetaMask');
+        setAddress('Please install MetaMask');
+
       }
     } catch (err) {
-      console.log('Please connect your Wallet')
       setAddress("Please connect your Wallet");
-
-      // console.error(err);
     }
   }
-
-  // const connectWallet = async () => {
-  //   if (window.ethereum) {
-  //     try {
-  //       await window.ethereum.request({ method: 'eth_requestAccounts' });
-  //       const provider = new window.ethers.providers.Web3Provider(window.ethereum);
-  //       const signer = provider.getSigner();
-  //       const connectedAddress = await signer.getAddress();
-  //       setAddress(connectedAddress);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   } else {
-  //     console.log('Metamask not detected');
-  //   }
-  // };
-
-
 
   return (
     <div>
       <p>Reactiveness test gareko: {meroAddress}</p>
       <p>Address: {address}</p>
+      <p>Provider: {providerStats}</p>
       <button onClick={reactiveness}>Test Reactiveness</button><br></br>
       <button onClick={connectWallet}>Connect Wallet</button><br></br>
       <button>Increment Count</button>
