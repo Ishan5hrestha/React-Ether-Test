3d2cc4fdf89b71dd0f60cb9d3ff1bc78b9100ac8
ishan5hrestha <ishanshrestha@gmail.com>
2023-02-24 18:06:17 +0545

    added contract code

---
+++ src/contracts/count.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+
+contract Count {
+    uint256 count;
+
+    function  getCount() view public returns(uint256){
+        return count;
+    }
+
+    function incrementCount()  public {
+        count += 1;
+    }
+
+      function decrementCount()  public {
+        count -= 1;
+    }
+}
