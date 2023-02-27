b3768e2f9e958ebb6142622b74516846fe695f2b
ishan5hrestha <ishanshrestha@gmail.com>
2023-02-28 00:41:55 +0545

    mapping and address

--- src/contracts/zombieContract.sol
+++ src/contracts/zombieContract.sol
@@ -1,11 +1,11 @@
 // Practise from cryptozombies.io Chapter: 1, Lesson: 1-13
-// Practise from cryptozombies.io Chapter: 2, Lesson:1
+// Practise from cryptozombies.io Chapter: 2, Lesson: 2
 // My zombie link: https://share.cryptozombies.io/en/lesson/1/share/dabaiZombie?id=Y3p8NTc1NzAy
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
-    // declare our event here
+    event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -17,9 +17,12 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
-        // and fire it here
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
